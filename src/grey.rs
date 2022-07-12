f9ba7396f70cab78051f2a2272d81f941f94f09e
Lukasz Gintowt <syzer3@gmail.com>
2022-07-12 14:04:16 +0200

    Make gray bot sort of work

--- src/grey.rs
+++ src/grey.rs
@@ -1,4 +1,5 @@
 use battle_bots_engine::*;
+use std::num::Wrapping;
 
 /** 
  * The grey bot is broken! It's using all the functions below, but they seem not to be implemented correctly
@@ -16,12 +17,21 @@ use battle_bots_engine::*;
 // Returns the position that's adjacent to the given one in the given direction, in the form (x, y)
 // eg. adjacent_position_in_direction(4, 5, Direction::Down) == (4, 6)
 pub fn adjacent_position_in_direction(x: usize, y: usize, direction: Direction) -> (usize, usize) {
-    (0, 0)
+    if x == 0 || y == 0 {
+        return (1, 1);
+    }
+    match direction {
+        Direction::Up => (x, y - 1),
+        Direction::Down => (x, y + 1),
+        Direction::Left => (x - 1, y),
+        Direction::Right => (x + 1, y),
+    }
 }
 
 // Returns whether there is a bot in the given position
 pub fn is_bot(game_state: &GameState, position: &Position) -> bool {
-    false
+    true
+    // game_state.
 }
 
 // Returns the shortest way to rotate the "from" direction to get the "to" direction
